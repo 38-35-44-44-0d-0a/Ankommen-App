f7ed94a5a6726fb039646ba0c1a0e01ded2182ea
38-35-44-44-0d-0a <root@killah1312.esy.es>
2016-06-09 13:30:45 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'ankommen-dev',
+		wp_ws_url : 'http://app.serviceikoe.de/wp-appkit-api/ankommen-dev',
+		theme : 'wpak-off-canvas-android',
+		version : '1.0.2',
+		app_title : 'Ankommen und mehr',
+		app_platform : 'android',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 's|Jn.<2^c=yRXj_5yu5*Pd]B0F#wAGn7E:&QT619Q-i,uJ?pBQxp7W4E{6>?[.S!',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
